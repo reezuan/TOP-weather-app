b4e2ab89b88b4f2c14fdd742c32f224ef71d1b76
Rizuan <john.lee.coco@gmail.com>
2024-06-07 01:24:40 +0800

    Create JS module for weather overview component

---
+++ src/components/current-weather/weatherOverview.js
@@ -0,0 +1,52 @@
+import Sun from "../../assets/sun.png";
+import Moon from "../../assets/moon.png";
+
+function weatherOverview() {
+    const container = document.createElement("section");
+    container.classList.add("current-weather-overview");
+    
+    // ----- ICON -----------------------------------------------------
+    const icon = new Image();
+    icon.setAttribute("data-is-day", "");
+
+    // Listens to data attribute change, then changes icon as needed.
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.type === "attributes") {
+                console.log("Day/night changed");
+                
+                if (icon.getAttribute("data-is-day") === "true") {
+                    icon.src = Sun;
+                } else {
+                    icon.src = Moon;
+                }
+            }
+        });
+    });
+
+    observer.observe(icon, {
+        attributes: true
+    });
+    
+    // ----- CURRENT TEMPERATURE --------------------------------------
+    const temperature = document.createElement("span");
+    temperature.setAttribute("data-current-temp", "");
+
+    const unit = document.createElement("span");
+    unit.setAttribute("data-temperature-unit", "");
+
+    const currentTemp = document.createElement("p");
+    currentTemp.append(temperature, unit);
+    
+    // ----- GENERAL FORECAST -----------------------------------------
+    const generalForecast = document.createElement("p");
+    generalForecast.setAttribute("data-general-forecast", "");
+
+    container.appendChild(icon);
+    container.appendChild(currentTemp);
+    container.appendChild(generalForecast);
+
+    return container;
+}
+
+export { weatherOverview };
---
+++ src/components/mainWeatherSection.js
@@ -0,0 +1,32 @@
+import { weatherHeading } from "./weatherHeading.js";
+import { weatherOverview } from "./current-weather/weatherOverview.js";
+import { feelsLikeCard } from "./current-weather/feelsLikeCard.js";
+import { chanceOfRainCard } from "./current-weather/chanceOfRainCard.js";
+import { humidityCard } from "./current-weather/humidityCard.js";
+import { UVCard } from "./current-weather/UVCard.js";
+import { windDirectionCard } from "./current-weather/windDirectionCard.js";
+import { windSpeedCard } from "./current-weather/windSpeedCard.js";
+
+function mainWeatherSection() {
+    const weatherSection = document.createElement("main");
+
+    // Key information
+    const weatherDetails = document.createElement("section");
+    weatherDetails.classList.add("current-weather-details");
+
+    weatherDetails.appendChild(feelsLikeCard());
+    weatherDetails.appendChild(windDirectionCard());            // Feels like           |       Wind direction
+    weatherDetails.appendChild(humidityCard());                 // Humidity             |       Wind speed
+    weatherDetails.appendChild(windSpeedCard());                // Chance of rain       |       UV
+    weatherDetails.appendChild(chanceOfRainCard());
+    weatherDetails.appendChild(UVCard());
+
+    // ----------------------------------------------------------------
+    weatherSection.appendChild(weatherHeading());
+    weatherSection.appendChild(weatherOverview());
+    weatherSection.appendChild(weatherDetails);
+
+    return weatherSection;
+}
+
+export { mainWeatherSection };
